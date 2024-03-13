1a1206bcfac00d7a2164cf2baacf91b26dab077c
Banda06 <williamntsikelelo@gmail.com>
2024-03-13 13:53:24 -0700

    Print all the letters except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Print the alphabet in lowercase, not q and e."""
-
+'''Print the alphabet in lowercase, not q and e.'''
 
 for i in range(97, 123):
     if i != 101 and i != 113:
