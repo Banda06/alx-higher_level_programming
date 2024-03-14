ac7e633c98ab766622fbc0ff16324626e019043e
Banda06 <williamntsikelelo@gmail.com>
2024-03-14 14:48:27 -0700

    prints a combo of numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(100):
+
     if i != 99:
-    print("{:d}{:d}".format(i // 10, i % 10), end=", ")
+        '''prints a combo of numbers'''
+        print("{:d}{:d}".format(i // 10, i % 10), end=", ")
     else:
-    print("{}".format(i))
+        print("{}".format(i))
