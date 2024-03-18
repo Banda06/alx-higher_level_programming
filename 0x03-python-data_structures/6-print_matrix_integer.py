c4f4e2637c2914943ff36b99cd6973e26ef7aaa6
Banda06 <williamntsikelelo@gmail.com>
2024-03-18 16:40:35 -0700

    prints a metrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+def print_matrix_integer(matrix=[[]]):
+    '''function that prints a matrix of integer'''
+
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            if column != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[row][column]), end='')
+        print()
