aba3ac3b0d6ce88bbb48ce67d54f564bf50403ac
Banda06 <williamntsikelelo@gmail.com>
2024-04-18 08:56:35 -0700

    Python inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A Square class shape, inheirts from Rectangle
+    """
+    def __init__(self, size):
+        """"
+        Init function for Square
+
+        Attributes:
+            size (int): The size of the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        str funtion to print width/height
+
+        Returns:
+            Return width/height
+        """
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
+
+    def area(self):
+        """
+        A function that calculates the area of the Square
+        """
+        return self.__size ** 2
