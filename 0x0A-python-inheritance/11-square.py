2bd5a7024979eea7194d43727970bde7686499a9
Banda06 <williamntsikelelo@gmail.com>
2024-04-18 09:03:26 -0700

    Python inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Defines a Rectangle subclass Square."""
-Rectangle = __import__('9-rectangle.py').Rectangle
 
 
-class Square(Rectangle):
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
     """
-    A Square class shape, inheirts from Rectangle
+    A Square class shape, inheirts from BaseGeometry
     """
     def __init__(self, size):
         """"
@@ -19,7 +21,7 @@ class Square(Rectangle):
 
     def __str__(self):
         """
-        str funtion to print width/height
+        str funtion to print with/height
 
         Returns:
             Return width/height
