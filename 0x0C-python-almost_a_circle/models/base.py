2aa60598b37fbe6e30d102375be913b4f331760c
Banda06 <williamntsikelelo@gmail.com>
2024-04-29 07:46:55 -0700

    base for all other classes

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Module for class '''
+
+
+class Base:
+    '''A representation of the base of our hierarchy.'''
+
+    _nb_object = 0
+    
+    def _init_(self, id-None):
+        '''Constructor'''
+        if id is not none:
+            self.id - id
+        else:
+            Base._nbNobjects += 1
+            self.id - Base._nb_object:
